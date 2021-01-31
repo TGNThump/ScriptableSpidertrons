4c7cb70b6f3b97d5cd903d9478a6d0d4d3eab809
Ben Pilgrim <ben@pilgrim.me.uk>
2021-01-31 13:49:59 +0000

    Initial commit.

---
+++ .github/ISSUE_TEMPLATE/bug-report.md
@@ -0,0 +1,16 @@
+---
+name: Bug Report
+about: Submit a bug report
+title: Bug Report
+labels: Bug
+assignees: ''
+
+---
+
+### Overview
+[an overview of the problem]
+
+### Enviroment
+- Factorio [x.x.x]
+- Scriptable Spidertrons [x.x.x]
+- [list any other relevant mods you had installed here]
---
+++ bin/build.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const packageJson = require("../package.json");
+const { writeFile, copyFile, mkdir, rmdir } = require("fs");
+const { spawn } = require("child_process");
+const { ncp } = require('ncp');
+const { promisify }  = require('util');
+const zip = require('bestzip');
+const core = require('@actions/core');
+
+let path = `dist/${packageJson.name}_${packageJson.version}`;
+
+(async () => {
+
+    await promisify(rmdir)(path, {recursive: true});
+    await promisify(mkdir)(path, {recursive: true});
+
+    await Promise.all([
+        new Promise((resolve, reject) => {
+            spawn(/^win/.test(process.platform) ? 'npx.cmd' : 'npx', ['tstl', '-p', 'tsconfig.json', '--outDir', path], {stdio: "inherit"}).on('close', code => {
+                if (code !== 0) reject(code);
+                else resolve();
+            });
+        }),
+        promisify(writeFile)(path + '/info.json', JSON.stringify({
+            name: packageJson.name,
+            version: packageJson.version,
+            title: packageJson.config.title,
+            author: packageJson.author,
+            contact: packageJson.bugs,
+            description: packageJson.description,
+            factorio_version: packageJson.config.factorio_version,
+        })),
+        promisify(ncp)('public', path),
+        promisify(copyFile)('changelog.txt', path + '/changelog.txt'),
+        promisify(copyFile)('README.md', path + '/README.md'),
+        promisify(copyFile)('LICENSE.md', path + '/LICENSE.md')
+    ]);
+
+
+    await zip({source: `${packageJson.name}_${packageJson.version}`, destination: `${packageJson.name}_${packageJson.version}.zip`, cwd: 'dist'})
+
+    core.setOutput('asset_path', `dist/${packageJson.name}_${packageJson.version}.zip`);
+    core.setOutput('asset_name', `${packageJson.name}_${packageJson.version}.zip`)
+
+    if (process.argv.includes('install')) {
+        console.log(`\nInstalling to ${process.env.APPDATA}/Factorio/mods`)
+        await promisify(ncp)(`dist/${packageJson.name}_${packageJson.version}`, `${process.env.APPDATA}/Factorio/mods/${packageJson.name}_${packageJson.version}`);
+    }
+})();
